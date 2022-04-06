72b794e83a97c1436ac9150a3e21deb9d2963a42
Mateus <Mateus@DESKTOP-AJ7HGJR>
2022-04-06 00:16:30 -0300

    README.md e Swagger incluídos.

---
+++ README.md
@@ -0,0 +1,45 @@
+# Desafio programação Bycoders_
+
+### Pre requisitos para instalação
+
+Docker, Docker compose
+
+
+### 🔧 Instalação
+
+No diretório raiz execute o comando
+    docker-compose up
+
+
+### 🔧 Execução da aplicação
+    
+Acesse a aplicação através do endereço:
+    http://localhost:9000
+
+Faça o upload do arquivo CNAB.txt pela interface gráfica na seção upload de transações.
+Visualize as lojas, os saldos das lojs e as transações de cada loja na seção lojas e transações.
+
+    
+
+### Pre requisitos para os testes
+
+.Net Core 3.1 SDK ou mais recente 
+
+
+## ⚙️ Executando os testes
+
+No diretorio raiz execute o comando
+    cd backend\tests\servicesTests
+    dotnet test
+
+
+
+## 📋 Documentação da API
+
+Acesse os endereços abaixo para visualizar a documentação dos métodos da API
+    Swagger UI: http://localhost:5000/swagger/index.html
+    Swagger Json: http://localhost:5000/swagger/v1/swagger.json
+
+
+
+* Obrigado 
--- backend/hosting/netcorewebapi/Startup.cs
+++ backend/hosting/netcorewebapi/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,6 +40,11 @@ namespace netcorewebapi
                                   });
             });
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Bycoders_test API", Version = "v1" });
+            });
+
             services.AddControllers();
 
             var connectionString = $"Server=mssql-server, 1433;Initial Catalog=bycoders;User ID=SA;Password=senhadificil.123";
@@ -51,6 +57,12 @@ namespace netcorewebapi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bycoders_test API V1");
+            });
+
             using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
             {
                 var context = serviceScope.ServiceProvider.GetRequiredService<LojaContext>();
